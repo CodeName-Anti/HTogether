d26eb99ba83d899849f97f2bef03c5cc0da0199b
JNNJ <95310130+CodeName-Anti@users.noreply.github.com>
2024-08-30 15:27:43 +0200

    Change default font to roboto + add FontAwesome6

--- HTogether/Rendering/GUIRenderer.cs
+++ HTogether/Rendering/GUIRenderer.cs
@@ -1,12 +1,15 @@
 ﻿using HarmonyLib;
 using HTogether.Utils;
+using IconFonts;
 using ImGuiNET;
 using SharpGUI;
 using System;
 using System.Collections.Generic;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Numerics;
+using System.Reflection;
 
 namespace HTogether.Rendering;
 
@@ -54,12 +57,13 @@ public class GUIRenderer
 		}
 
 		GUI.OnRender += OnRender;
-		GUI.OnInitImGui += SetupImGuiStyle;
+		GUI.OnInitImGui += InitImGui;
 		GUI.Initialize();
 
 		GUI.HandleInput = true;
 		GUI.BlockInput = false;
 	}
+
 
 	public void Shutdown()
 	{
@@ -71,6 +75,35 @@ public class GUIRenderer
 		GUITab tab = new(name, Tabs.Keys.Last() + 1);
 		Tabs.Add(tab.Id, tab);
 		return tab.Id;
+	}
+	
+	private unsafe void InitImGui()
+	{
+		try
+		{
+			ImGuiStyles.SetupFutureDarkStyle();
+
+			string assetsPath = typeof(HTogether).Namespace + ".Assets";
+			Assembly assembly = Assembly.GetExecutingAssembly();
+
+			ImFontAtlasPtr fonts = ImGui.GetIO().Fonts;
+
+			float baseFontSize = 15f;
+			float iconFontSize = baseFontSize / 3 * 3.5f;
+
+			ImFontPtr robotoFont = fonts.LoadFontFromResources(assetsPath + ".Roboto.Roboto-Regular.ttf", assembly, baseFontSize);
+			ImGui.GetIO().NativePtr->FontDefault = robotoFont.NativePtr;
+
+			(uint, uint) fontAwesomeRange = (FontAwesome6.IconMin, FontAwesome6.IconMax16);
+			fonts.LoadIconFontFromResources(assetsPath + ".FontAwesome." + FontAwesome6.FontIconFileNameFAR, assembly, iconFontSize, fontAwesomeRange);
+			fonts.LoadIconFontFromResources(assetsPath + ".FontAwesome." + FontAwesome6.FontIconFileNameFAS, assembly, iconFontSize, fontAwesomeRange);
+
+			fonts.Build();
+
+		} catch (Exception ex)
+		{
+			HTogether.Logger.LogError($"Failed to init ImGui {ex}");
+		}
 	}
 
 	private void RenderIntro()
@@ -177,93 +210,4 @@ public class GUIRenderer
 
 		ImGui.End();
 	}
-
-	private static void SetupImGuiStyle()
-	{
-		// Future Dark stylerewrking from ImThemes
-		var style = ImGui.GetStyle();
-
-		style.Alpha = 1.0f;
-		style.DisabledAlpha = 1.0f;
-		style.WindowPadding = new Vector2(12.0f, 12.0f);
-		style.WindowRounding = 0.0f;
-		style.WindowBorderSize = 0.0f;
-		style.WindowMinSize = new Vector2(20.0f, 20.0f);
-		style.WindowTitleAlign = new Vector2(0.5f, 0.5f);
-		style.WindowMenuButtonPosition = ImGuiDir.None;
-		style.ChildRounding = 0.0f;
-		style.ChildBorderSize = 1.0f;
-		style.PopupRounding = 0.0f;
-		style.PopupBorderSize = 1.0f;
-		style.FramePadding = new Vector2(6.0f, 6.0f);
-		style.FrameRounding = 0.0f;
-		style.FrameBorderSize = 0.0f;
-		style.ItemSpacing = new Vector2(12.0f, 6.0f);
-		style.ItemInnerSpacing = new Vector2(6.0f, 3.0f);
-		style.CellPadding = new Vector2(12.0f, 6.0f);
-		style.IndentSpacing = 20.0f;
-		style.ColumnsMinSpacing = 6.0f;
-		style.ScrollbarSize = 12.0f;
-		style.ScrollbarRounding = 0.0f;
-		style.GrabMinSize = 12.0f;
-		style.GrabRounding = 0.0f;
-		style.TabRounding = 0.0f;
-		style.TabBorderSize = 0.0f;
-		style.TabMinWidthForCloseButton = 0.0f;
-		style.ColorButtonPosition = ImGuiDir.Right;
-		style.ButtonTextAlign = new Vector2(0.5f, 0.5f);
-		style.SelectableTextAlign = new Vector2(0.0f, 0.0f);
-
-		style.Colors[(int)ImGuiCol.Text] = new Vector4(1.0f, 1.0f, 1.0f, 1.0f);
-		style.Colors[(int)ImGuiCol.TextDisabled] = new Vector4(0.2745098173618317f, 0.3176470696926117f, 0.4509803950786591f, 1.0f);
-		style.Colors[(int)ImGuiCol.WindowBg] = new Vector4(0.0784313753247261f, 0.08627451211214066f, 0.1019607856869698f, 1.0f);
-		style.Colors[(int)ImGuiCol.ChildBg] = new Vector4(0.0784313753247261f, 0.08627451211214066f, 0.1019607856869698f, 1.0f);
-		style.Colors[(int)ImGuiCol.PopupBg] = new Vector4(0.0784313753247261f, 0.08627451211214066f, 0.1019607856869698f, 1.0f);
-		style.Colors[(int)ImGuiCol.Border] = new Vector4(0.1568627506494522f, 0.168627455830574f, 0.1921568661928177f, 1.0f);
-		style.Colors[(int)ImGuiCol.BorderShadow] = new Vector4(0.0784313753247261f, 0.08627451211214066f, 0.1019607856869698f, 1.0f);
-		style.Colors[(int)ImGuiCol.FrameBg] = new Vector4(0.1176470592617989f, 0.1333333402872086f, 0.1490196138620377f, 1.0f);
-		style.Colors[(int)ImGuiCol.FrameBgHovered] = new Vector4(0.1568627506494522f, 0.168627455830574f, 0.1921568661928177f, 1.0f);
-		style.Colors[(int)ImGuiCol.FrameBgActive] = new Vector4(0.2352941185235977f, 0.2156862765550613f, 0.5960784554481506f, 1.0f);
-		style.Colors[(int)ImGuiCol.TitleBg] = new Vector4(0.0470588244497776f, 0.05490196123719215f, 0.07058823853731155f, 1.0f);
-		style.Colors[(int)ImGuiCol.TitleBgActive] = new Vector4(0.0470588244497776f, 0.05490196123719215f, 0.07058823853731155f, 1.0f);
-		style.Colors[(int)ImGuiCol.TitleBgCollapsed] = new Vector4(0.0784313753247261f, 0.08627451211214066f, 0.1019607856869698f, 1.0f);
-		style.Colors[(int)ImGuiCol.MenuBarBg] = new Vector4(0.09803921729326248f, 0.105882354080677f, 0.1215686276555061f, 1.0f);
-		style.Colors[(int)ImGuiCol.ScrollbarBg] = new Vector4(0.0470588244497776f, 0.05490196123719215f, 0.07058823853731155f, 1.0f);
-		style.Colors[(int)ImGuiCol.ScrollbarGrab] = new Vector4(0.1176470592617989f, 0.1333333402872086f, 0.1490196138620377f, 1.0f);
-		style.Colors[(int)ImGuiCol.ScrollbarGrabHovered] = new Vector4(0.1568627506494522f, 0.168627455830574f, 0.1921568661928177f, 1.0f);
-		style.Colors[(int)ImGuiCol.ScrollbarGrabActive] = new Vector4(0.1176470592617989f, 0.1333333402872086f, 0.1490196138620377f, 1.0f);
-		style.Colors[(int)ImGuiCol.CheckMark] = new Vector4(0.4980392158031464f, 0.5137255191802979f, 1.0f, 1.0f);
-		style.Colors[(int)ImGuiCol.SliderGrab] = new Vector4(0.4980392158031464f, 0.5137255191802979f, 1.0f, 1.0f);
-		style.Colors[(int)ImGuiCol.SliderGrabActive] = new Vector4(0.5372549295425415f, 0.5529412031173706f, 1.0f, 1.0f);
-		style.Colors[(int)ImGuiCol.Button] = new Vector4(0.1176470592617989f, 0.1333333402872086f, 0.1490196138620377f, 1.0f);
-		style.Colors[(int)ImGuiCol.ButtonHovered] = new Vector4(0.196078434586525f, 0.1764705926179886f, 0.5450980663299561f, 1.0f);
-		style.Colors[(int)ImGuiCol.ButtonActive] = new Vector4(0.2352941185235977f, 0.2156862765550613f, 0.5960784554481506f, 1.0f);
-		style.Colors[(int)ImGuiCol.Header] = new Vector4(0.1176470592617989f, 0.1333333402872086f, 0.1490196138620377f, 1.0f);
-		style.Colors[(int)ImGuiCol.HeaderHovered] = new Vector4(0.196078434586525f, 0.1764705926179886f, 0.5450980663299561f, 1.0f);
-		style.Colors[(int)ImGuiCol.HeaderActive] = new Vector4(0.2352941185235977f, 0.2156862765550613f, 0.5960784554481506f, 1.0f);
-		style.Colors[(int)ImGuiCol.Separator] = new Vector4(0.1568627506494522f, 0.1843137294054031f, 0.250980406999588f, 1.0f);
-		style.Colors[(int)ImGuiCol.SeparatorHovered] = new Vector4(0.1568627506494522f, 0.1843137294054031f, 0.250980406999588f, 1.0f);
-		style.Colors[(int)ImGuiCol.SeparatorActive] = new Vector4(0.1568627506494522f, 0.1843137294054031f, 0.250980406999588f, 1.0f);
-		style.Colors[(int)ImGuiCol.ResizeGrip] = new Vector4(0.1176470592617989f, 0.1333333402872086f, 0.1490196138620377f, 1.0f);
-		style.Colors[(int)ImGuiCol.ResizeGripHovered] = new Vector4(0.196078434586525f, 0.1764705926179886f, 0.5450980663299561f, 1.0f);
-		style.Colors[(int)ImGuiCol.ResizeGripActive] = new Vector4(0.2352941185235977f, 0.2156862765550613f, 0.5960784554481506f, 1.0f);
-		style.Colors[(int)ImGuiCol.Tab] = new Vector4(0.0470588244497776f, 0.05490196123719215f, 0.07058823853731155f, 1.0f);
-		style.Colors[(int)ImGuiCol.TabHovered] = new Vector4(0.1176470592617989f, 0.1333333402872086f, 0.1490196138620377f, 1.0f);
-		style.Colors[(int)ImGuiCol.PlotLines] = new Vector4(0.5215686559677124f, 0.6000000238418579f, 0.7019608020782471f, 1.0f);
-		style.Colors[(int)ImGuiCol.PlotLinesHovered] = new Vector4(0.03921568766236305f, 0.9803921580314636f, 0.9803921580314636f, 1.0f);
-		style.Colors[(int)ImGuiCol.PlotHistogram] = new Vector4(1.0f, 0.2901960909366608f, 0.5960784554481506f, 1.0f);
-		style.Colors[(int)ImGuiCol.PlotHistogramHovered] = new Vector4(0.9960784316062927f, 0.4745098054409027f, 0.6980392336845398f, 1.0f);
-		style.Colors[(int)ImGuiCol.TableHeaderBg] = new Vector4(0.0470588244497776f, 0.05490196123719215f, 0.07058823853731155f, 1.0f);
-		style.Colors[(int)ImGuiCol.TableBorderStrong] = new Vector4(0.0470588244497776f, 0.05490196123719215f, 0.07058823853731155f, 1.0f);
-		style.Colors[(int)ImGuiCol.TableBorderLight] = new Vector4(0.0f, 0.0f, 0.0f, 1.0f);
-		style.Colors[(int)ImGuiCol.TableRowBg] = new Vector4(0.1176470592617989f, 0.1333333402872086f, 0.1490196138620377f, 1.0f);
-		style.Colors[(int)ImGuiCol.TableRowBgAlt] = new Vector4(0.09803921729326248f, 0.105882354080677f, 0.1215686276555061f, 1.0f);
-		style.Colors[(int)ImGuiCol.TextSelectedBg] = new Vector4(0.2352941185235977f, 0.2156862765550613f, 0.5960784554481506f, 1.0f);
-		style.Colors[(int)ImGuiCol.DragDropTarget] = new Vector4(0.4980392158031464f, 0.5137255191802979f, 1.0f, 1.0f);
-		style.Colors[(int)ImGuiCol.NavHighlight] = new Vector4(0.4980392158031464f, 0.5137255191802979f, 1.0f, 1.0f);
-		style.Colors[(int)ImGuiCol.NavWindowingHighlight] = new Vector4(0.4980392158031464f, 0.5137255191802979f, 1.0f, 1.0f);
-		style.Colors[(int)ImGuiCol.NavWindowingDimBg] = new Vector4(0.196078434586525f, 0.1764705926179886f, 0.5450980663299561f, 0.501960813999176f);
-		style.Colors[(int)ImGuiCol.ModalWindowDimBg] = new Vector4(0.196078434586525f, 0.1764705926179886f, 0.5450980663299561f, 0.501960813999176f);
-	}
-
 }
--- HTogether/Utils/ImGuiExtensions.cs
+++ HTogether/Utils/ImGuiExtensions.cs
@@ -6,6 +6,10 @@ using SysVec3 = System.Numerics.Vector3;
 using SysVec4 = System.Numerics.Vector4;
 
 using SysColor = System.Drawing.Color;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Reflection;
+using System;
 
 namespace HTogether.Utils;
 
@@ -45,6 +49,72 @@ public static class ImGuiExtensions
 	public static uint ToImGuiColor(this Color color)
 	{
 		return color.ToSysVec().ToImguiColor();
+	}
+
+	private static IntPtr GetFontDataFromResources(string resourceName, Assembly assembly, out int fontDataLength)
+	{
+		using Stream fontStream = assembly.GetManifestResourceStream(resourceName);
+
+		byte[] fontData = new byte[fontStream.Length];
+		fontStream.Read(fontData, 0, (int)fontStream.Length);
+
+		IntPtr fontPtr = ImGui.MemAlloc((uint)fontData.Length);
+		Marshal.Copy(fontData, 0, fontPtr, fontData.Length);
+
+		fontDataLength = fontData.Length;
+
+		return fontPtr;
+	}
+
+	public static unsafe ImFontPtr LoadFontFromResources(this ImFontAtlasPtr fontAtlas, string resourceName, Assembly assembly, float fontSize)
+	{
+		IntPtr fontPtr = GetFontDataFromResources(resourceName, assembly, out int fontDataLength);
+
+		return fontAtlas.AddFontFromMemoryTTF(fontPtr, fontDataLength, fontSize);
+	}
+
+	public static unsafe ImFontPtr LoadFontFromResources(this ImFontAtlasPtr fontAtlas, string resourceName, Assembly assembly, float fontSize, ImFontConfigPtr fontConfig)
+	{
+		IntPtr fontPtr = GetFontDataFromResources(resourceName, assembly, out int fontDataLength);
+
+		return fontAtlas.AddFontFromMemoryTTF(fontPtr, fontDataLength, fontSize, fontConfig);
+	}
+
+	public static unsafe ImFontPtr LoadFontFromResources(this ImFontAtlasPtr fontAtlas, string resourceName, Assembly assembly, float fontSize, ImFontConfigPtr fontConfig, IntPtr glyphRanges)
+	{
+		IntPtr fontPtr = GetFontDataFromResources(resourceName, assembly, out int fontDataLength);
+
+		return fontAtlas.AddFontFromMemoryTTF(fontPtr, fontDataLength, fontSize, fontConfig, glyphRanges);
+	}
+
+	public static unsafe ImFontPtr LoadIconFontFromResources(this ImFontAtlasPtr fontAtlas, string resourceName, Assembly assembly, float size, (uint, uint) range)
+	{
+		ImFontConfigPtr configuration = ImGuiNative.ImFontConfig_ImFontConfig();
+
+		configuration.MergeMode = true;
+		configuration.PixelSnapH = true;
+		configuration.GlyphOffset = new SysVec2(0, 1);
+
+		GCHandle rangeHandle = GCHandle.Alloc(new uint[]
+		{
+			range.Item1,
+			range.Item2,
+			0
+		}, GCHandleType.Pinned);
+
+		try
+		{
+			return fontAtlas.LoadFontFromResources(resourceName, assembly, size, configuration, rangeHandle.AddrOfPinnedObject());
+		}
+		finally
+		{
+			configuration.Destroy();
+
+			if (rangeHandle.IsAllocated)
+			{
+				rangeHandle.Free();
+			}
+		}
 	}
 
 }
