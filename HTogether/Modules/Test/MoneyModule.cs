43025f69f30813e433d0722fb2aba5e74be99d24
JNNJ <95310130+CodeName-Anti@users.noreply.github.com>
2024-08-31 22:35:11 +0200

    Make Amount in MoneyModule public

--- HTogether/Modules/Test/MoneyModule.cs
+++ HTogether/Modules/Test/MoneyModule.cs
@@ -8,24 +8,24 @@ namespace HTogether.Modules;
 [HModule]
 public class MoneyModule() : Module("test", BaseTabID.Money)
 {
-	private int amount;
+	public int Amount;
 
 	public override void RenderGUIElements()
 	{
-		ImGui.TextColored(Color.Red.ToSysVec(), "Host only");
+		ImGui.SliderInt("Money", ref Amount, 0, 10000);
 
-		ImGui.SliderInt("Money", ref amount, 0, 10000);
+		ImGui.SeparatorText("Host only");
 
 		if (ImGui.Button("Add"))
 		{
-			GameData.Instance.NetworkgameFunds += amount;
+			GameData.Instance.NetworkgameFunds += Amount;
 		}
 
 		ImGui.SameLine();
 
 		if (ImGui.Button("Remove"))
 		{
-			GameData.Instance.NetworkgameFunds -= amount;
+			GameData.Instance.NetworkgameFunds -= Amount;
 		}
 	}
 
